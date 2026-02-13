d343f91ce3dc416057415b8207743393eb42a2f2
Kavya1645 <kavyashah1645@gmail.com>
2026-02-13 17:52:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
+import sqlite3
+from datetime import datetime
 import streamlit as st
 import numpy as np
 import joblib
+
+conn = sqlite3.connect("patients.db")
+c = conn.cursor()
+
+c.execute("""
+CREATE TABLE IF NOT EXISTS patient_data (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    age INTEGER,
+    sex INTEGER,
+    cp INTEGER,
+    trestbps INTEGER,
+    chol INTEGER,
+    prediction INTEGER,
+    timestamp TEXT
+)
+""")
+
+conn.commit()
 
 model = joblib.load("heart_model.pkl")
 scaler = joblib.load("scaler.pkl")
@@ -22,14 +42,29 @@ ca = st.number_input("Number of Major Vessels (0-3)", 0,3)
 thal = st.number_input("Thal (0-3)", 0,3)
 
 if st.button("Predict"):
+
     input_data = np.array([[age, sex, cp, trestbps, chol, fbs,
                             restecg, thalach, exang, oldpeak,
                             slope, ca, thal]])
-    
+
     input_data = scaler.transform(input_data)
     prediction = model.predict(input_data)
 
-    if prediction[0] == 1:
+    result = int(prediction[0])
+
+    # Save to database
+    c.execute("""
+        INSERT INTO patient_data (age, sex, cp, trestbps, chol, prediction, timestamp)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    """, (age, sex, cp, trestbps, chol, result, datetime.now()))
+
+    conn.commit()
+
+    if result == 1:
         st.error("High Risk of Heart Disease")
     else:
         st.success("Low Risk of Heart Disease")
+
+if st.checkbox("Show Patient History"):
+    records = c.execute("SELECT * FROM patient_data").fetchall()
+    st.write(records)
