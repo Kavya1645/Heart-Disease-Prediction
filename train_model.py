20699eded0bbead843525b41a24cff81ca2184a2
Kavya1645 <kavyashah1645@gmail.com>
2026-04-06 16:31:09 +0530

    Added multiple ML models and comparison metrices

--- train_model.py
+++ train_model.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import joblib
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 
 data = pd.read_csv("heart.csv")
 
@@ -18,14 +23,41 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-model = LogisticRegression()
-model.fit(X_train, y_train)
+models = {
+    "Logistic Regression": LogisticRegression(),
+    "Decision Tree": DecisionTreeClassifier(),
+    "SVM": SVC(),
+    "KNN": KNeighborsClassifier()
+}
 
-y_pred = model.predict(X_test)
+results = []
 
-print("Accuracy:", accuracy_score(y_test, y_pred))
+for name, model in models.items():
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
 
-joblib.dump(model, "heart_model.pkl")
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    cm = confusion_matrix(y_test, y_pred)
+
+    results.append([name, acc, prec, rec, f1])
+
+    print(f"\n{name}")
+    print("Accuracy:", acc)
+    print("Precision:", prec)
+    print("Recall:", rec)
+    print("F1 Score:", f1)
+    print("Confusion Matrix:\n", cm)
+
+joblib.dump(models["Logistic Regression"], "log_model.pkl")
+joblib.dump(models["Decision Tree"], "dt_model.pkl")
+joblib.dump(models["SVM"], "svm_model.pkl")
+joblib.dump(models["KNN"], "knn_model.pkl")
 joblib.dump(scaler, "scaler.pkl")
 
-print("Model Saved Successfully!")
+df = pd.DataFrame(results, columns=["Model", "Accuracy", "Precision", "Recall", "F1 Score"])
+df.to_csv("model_comparison.csv", index=False)
+
+print("\nComparison saved as model_comparison.csv")
